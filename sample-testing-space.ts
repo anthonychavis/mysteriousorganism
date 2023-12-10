de25bf3b32f3bf7747de94a69445a7e3c8c05e91
Anthony Chavis <gitanthony@yahoo.com>
2023-12-10 13:18:44 -0500

    testing new class & fxn

--- sample-testing-space.ts
+++ sample-testing-space.ts
@@ -1,31 +1,15 @@
-import { testSamples } from './mysterious-organism';
-// WORKS !! Obj of Objs for readability/portability (as long as mem can handle)
-// console.log(testSamples);
-const { sample1 } = testSamples;
-console.log(sample1);
-console.log(sample1.complementStrand);
+import { survivingOrganisms } from './mysterious-organism';
 
-const { sample30 } = testSamples;
+const { sample1 } = survivingOrganisms;
+const { sample30 } = survivingOrganisms;
 
-// compareTwoDNA(sample1.dna, sample30.dna);
-sample1.compareDNAWith(sample30);
-console.log(sample1.specimenNum);
-
-console.log(sample1.complementStrand);
+console.log('sample obj', sample1);
+console.log('sample complement strand', sample1.complementStrand);
+console.log(sample1.compareDNAWith(sample30));
+console.log('sample dna pre-mutation', sample1.dna);
 sample1.mutate();
-console.log(sample1.complementStrand);
-
-// CLASS ??
-// const thing1 = new testSamplesClass();
-// const thing2 = new testSamplesClass();
-
-// OLD Arr of Objs
-// console.log(testSamples[0]);
-// const zero = testSamples[0];
-// console.log('zero', zero);
-// const zero = testSamples[0].dna;
-// const one = testSamples[1].dna;
-// console.log('1 ', zero, '2 ', one);
-// console.log('1 ', zero, '2 ', testSamples[0].complementStrand);
-// console.log('3', testSamples[0].mutate());
-// console.log('1 ', zero, '2 ', testSamples[0].complementStrand);
+console.log(
+    'sample complement strand after mutation',
+    sample1.complementStrand
+);
+console.log('sample dna post-mutation', sample1.dna);
