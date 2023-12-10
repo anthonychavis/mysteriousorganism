1b4f63012ac74a2aa02de53289b886a7e7058738
Anthony Chavis <gitanthony@yahoo.com>
2023-12-10 12:23:05 -0500

    fxn to fill obj

--- mysterious-organism.ts
+++ mysterious-organism.ts
@@ -1,101 +1,14 @@
-export const testSamples = (() => {
-    interface OrganismObj {
-        specimenNum: number;
-        dna: string[];
-        _complementStrand: string[];
-        mutate(): string[];
-        compareDNAWith(pAequor: { dna: string[]; specimenNum: number }): void;
-        readonly complementStrand: string[];
-        setComplementStrand: () => void;
-        willLikelySurvive(): boolean;
-    }
-
-    // move into the obj/class ??
-    const dnaBases = ['A', 'T', 'C', 'G'];
-    const mockUpStrand = () => {
-        const newStrand: string[] = [];
-        for (let i = 0; i < 15; i++) {
-            newStrand.push(
-                dnaBases[Math.floor(Math.random() * dnaBases.length)]
-            );
-        }
-        return newStrand;
-    };
-    // organism object
-    const pAequorFactory = (num: number, dnaArr: string[]): OrganismObj => {
-        return {
-            // reflects number of completed iterations w/in survivingOrganisms() until this organism was created -- zero-based
-            specimenNum: num,
-            dna: dnaArr,
-            _complementStrand: [],
-            compareDNAWith(pAequor: { dna: string[]; specimenNum: number }) {
-                let count = 0;
-                for (let i = 0; i < this.dna.length; i++) {
-                    if (this.dna[i] === pAequor.dna[i]) count++;
-                }
-                console.log(
-                    `This specimen, #${this.specimenNum}, and specimen #${
-                        pAequor.specimenNum
-                    } have ${((count / this.dna.length) * 100).toFixed(
-                        2
-                    )}% DNA in common`
-                );
-                return;
-            },
-            get complementStrand() {
-                if (!this._complementStrand.length) this.setComplementStrand();
-                return this._complementStrand;
-            },
-            setComplementStrand() {
-                this._complementStrand = [];
-                for (let base of this.dna) {
-                    switch (base) {
-                        case 'A':
-                            this._complementStrand.push('T');
-                            break;
-                        case 'T':
-                            this._complementStrand.push('A');
-                            break;
-                        case 'C':
-                            this._complementStrand.push('G');
-                            break;
-                        case 'G':
-                            this._complementStrand.push('C');
-                            break;
-                        default:
-                            'bug in the code';
-                    }
-                }
-                return;
-            },
-            // mutate single dna pair
-            mutate() {
-                const baseIndex = Math.floor(Math.random() * this.dna.length);
-                const selectedBase = this.dna[baseIndex];
-                const newBases = dnaBases.filter(base => base !== selectedBase);
-                this.dna[baseIndex] =
-                    newBases[Math.floor(Math.random() * newBases.length)];
-                this.setComplementStrand();
-                return this.dna;
-            },
-            willLikelySurvive() {
-                let count = 0;
-                for (let base of this.dna) {
-                    if (base === 'C' || base === 'G') count++;
-                }
-                return count / this.dna.length >= 0.6;
-            },
-        };
-    };
-    // make object containing 30 organism objects
-    const survivingOrganisms: { [key: string]: OrganismObj } = {};
+import pAequor from './class-organism';
+// make object containing 30 organism objects
+const survivingOrganismsFill = () => {
+    const survivingOrganisms: { [key: string]: pAequor } = {};
     let sampleNum = 1;
     for (let i = 0; Object.keys(survivingOrganisms).length < 30; i++) {
-        let dna = mockUpStrand(),
-            org = pAequorFactory(i, dna);
+        let org = new pAequor(i);
         if (org.willLikelySurvive())
             survivingOrganisms[`sample${sampleNum++}`] = org;
     }
-    // samples named 1-30
     return survivingOrganisms;
-})();
+};
+const survivingOrganisms = survivingOrganismsFill();
+export { survivingOrganisms };
