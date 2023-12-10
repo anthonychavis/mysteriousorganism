5e6df2c493019c56260d0165d0a97e9e4d1243f0
Anthony Chavis <gitanthony@yahoo.com>
2023-12-10 12:49:49 -0500

    mockup private; static ranNum; mssg for mutation death

--- class-organism.ts
+++ class-organism.ts
@@ -2,7 +2,7 @@ export default class PAequor {
     // reflects number of completed iterations w/in survivingOrganisms() until this organism was created -- zero-based
     specimenNum: number;
     #dnaBases = ['A', 'T', 'C', 'G'];
-    #dna = this.mockUpStrand();
+    #dna = this.#mockUpStrand();
     #complementStrand: string[] = [];
     constructor(i: number) {
         this.specimenNum = i;
@@ -45,26 +45,30 @@ export default class PAequor {
         }
         return;
     }
-    mockUpStrand(): string[] {
+    #mockUpStrand(): string[] {
         const newStrand: string[] = [];
         for (let i = 0; i < 15; i++) {
             newStrand.push(
-                this.#dnaBases[
-                    Math.floor(Math.random() * this.#dnaBases.length)
-                ]
+                this.#dnaBases[PAequor.ranNum(this.#dnaBases.length)]
             );
         }
         return newStrand;
     }
     // mutate single dna pair
     mutate() {
-        const baseIndex = Math.floor(Math.random() * this.#dna.length);
+        const baseIndex = PAequor.ranNum(this.#dna.length);
         const selectedBase = this.#dna[baseIndex];
         const newBases = this.#dnaBases.filter(base => base !== selectedBase);
-        this.#dna[baseIndex] =
-            newBases[Math.floor(Math.random() * newBases.length)];
+        this.#dna[baseIndex] = newBases[PAequor.ranNum(newBases.length)];
+        if (!this.willLikelySurvive())
+            console.log(
+                'The mutation of a DNA base-pair has caused this organism to die.'
+            );
         this.setComplementStrand(this.#dna);
         return this.#dna;
+    }
+    static ranNum(multiplier: number) {
+        return Math.floor(Math.random() * multiplier);
     }
     willLikelySurvive() {
         let count = 0;
