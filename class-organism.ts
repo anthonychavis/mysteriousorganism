a9d88e96d055f01a566bc6316e3df5d7dac46b61
Anthony Chavis <gitanthony@yahoo.com>
2023-12-20 18:43:23 -0500

    Org class for testing

--- class-organism.ts
+++ class-organism.ts
@@ -1,11 +1,14 @@
+// for JSON data
 export class PAequorSamples {
     #dnaBases = ['A', 'T', 'C', 'G'];
-    _dna = this.#mockUpStrand();
-    dnaLength = this._dna.length;
+    _dna;
+    dnaLength;
     // reflects number of completed iterations w/in survivingOrganismsFill() until this organism was created -- zero-based
-    specimenNum: number;
+    specimenNum;
     constructor(i: number) {
         this.specimenNum = i;
+        this._dna = this.#mockUpStrand();
+        this.dnaLength = this._dna.length;
     }
     get dna() {
         return this._dna;
@@ -26,79 +29,78 @@ export class PAequorSamples {
     }
 }
 
-// export default class PAequorTesting {
-//     #complementStrand: string[] = [];
-//     #dnaBases = ['A', 'T', 'C', 'G'];
-//     _dna;
-//     dnaLength;
-//     // reflects number of completed iterations w/in survivingOrganismsFill() until this organism was created -- zero-based
-//     specimenNum: number;
-//     constructor(dna: string[], i: number, dnaLength) {
-//         this._dna = dna;
-//         this.dnaLength = dna.length;
-//         this.specimenNum = i;
-//         this.#setComplementStrand(this._dna);
-//     }
-//     compareDNAWith(pAequor: { dna: string[]; specimenNum: number }) {
-//         let count = 0;
-//         for (let i = 0; i < this.dnaLength; i++)
-//             this._dna[i] === pAequor.dna[i] && count++;
-//         return `This specimen, #${this.specimenNum}, and specimen #${
-//             pAequor.specimenNum
-//         } have ${((count / this.dnaLength) * 100).toFixed(2)}% DNA in common`;
-//     }
-//     get complementStrand() {
-//         return this.#complementStrand;
-//     }
-//     get dna() {
-//         return this._dna;
-//     }
-//     #mockUpStrand(): string[] {
-//         const newStrand: string[] = [];
-//         for (let i = 0; i < 15; i++)
-//             newStrand.push(this.#dnaBases[this.#ranNum(this.#dnaBases.length)]);
-//         return newStrand;
-//     }
-//     // mutate single dna pair
-//     mutate() {
-//         const baseIndex = this.#ranNum(this.dnaLength);
-//         const selectedBase = this._dna[baseIndex];
-//         const newBases = this.#dnaBases.filter(base => base !== selectedBase);
-//         this._dna[baseIndex] = newBases[this.#ranNum(newBases.length)];
-//         if (!this.willLikelySurvive())
-//             console.log(
-//                 'The mutation of a DNA base-pair has caused this organism to die.'
-//             );
-//         this.#setComplementStrand(this._dna);
-//         return this._dna;
-//     }
-//     #ranNum(multiplier: number) {
-//         return Math.floor(Math.random() * multiplier);
-//     }
-//     #setComplementStrand(dna: string[]) {
-//         this.#complementStrand = [];
-//         for (let base of dna)
-//             switch (base) {
-//                 case 'A':
-//                     this.#complementStrand.push('T');
-//                     break;
-//                 case 'T':
-//                     this.#complementStrand.push('A');
-//                     break;
-//                 case 'C':
-//                     this.#complementStrand.push('G');
-//                     break;
-//                 case 'G':
-//                     this.#complementStrand.push('C');
-//                     break;
-//                 default:
-//                     'bug in the code';
-//             }
-//         return;
-//     }
-//     willLikelySurvive() {
-//         let count = 0;
-//         for (let base of this._dna) if (base === 'C' || base === 'G') count++;
-//         return count / this.dnaLength >= 0.6;
-//     }
-// }
+// for experimenting w/samples
+export class PAequorTesting {
+    #complementStrand: string[] = [];
+    #dnaBases = ['A', 'T', 'C', 'G'];
+    _dna;
+    dnaLength;
+    // reflects number of completed iterations w/in survivingOrganismsFill() until this organism was created -- zero-based
+    specimenNum: number;
+    constructor(jsonSample: {
+        _dna: string[];
+        dnaLength: number;
+        specimenNum: number;
+    }) {
+        this._dna = jsonSample._dna;
+        this.dnaLength = jsonSample.dnaLength;
+        this.specimenNum = jsonSample.specimenNum;
+        this.#setComplementStrand(this._dna);
+    }
+    compareDNAWith(pAequor: { dna: string[]; specimenNum: number }) {
+        let count = 0;
+        for (let i = 0; i < this.dnaLength; i++)
+            this._dna[i] === pAequor.dna[i] && count++;
+        return `This specimen, #${this.specimenNum}, and specimen #${
+            pAequor.specimenNum
+        } have ${((count / this.dnaLength) * 100).toFixed(2)}% DNA in common`;
+    }
+    get complementStrand() {
+        return this.#complementStrand;
+    }
+    get dna() {
+        return this._dna;
+    }
+    // mutate single dna pair
+    mutate() {
+        const baseIndex = this.#ranNum(this.dnaLength);
+        const selectedBase = this._dna[baseIndex];
+        const newBases = this.#dnaBases.filter(base => base !== selectedBase);
+        this._dna[baseIndex] = newBases[this.#ranNum(newBases.length)];
+        if (!this.willLikelySurvive())
+            console.log(
+                'The mutation of a DNA base-pair has caused this organism to die.'
+            );
+        this.#setComplementStrand(this._dna);
+        return this._dna;
+    }
+    #ranNum(multiplier: number) {
+        return Math.floor(Math.random() * multiplier);
+    }
+    #setComplementStrand(dna: string[]) {
+        this.#complementStrand = [];
+        for (let base of dna)
+            switch (base) {
+                case 'A':
+                    this.#complementStrand.push('T');
+                    break;
+                case 'T':
+                    this.#complementStrand.push('A');
+                    break;
+                case 'C':
+                    this.#complementStrand.push('G');
+                    break;
+                case 'G':
+                    this.#complementStrand.push('C');
+                    break;
+                default:
+                    'bug in the code';
+            }
+        return;
+    }
+    willLikelySurvive() {
+        let count = 0;
+        for (let base of this._dna) if (base === 'C' || base === 'G') count++;
+        return count / this.dnaLength >= 0.6;
+    }
+}
